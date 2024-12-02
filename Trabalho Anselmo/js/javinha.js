741522a81e001ac4ab57a175c134f719cf4c5d56
MurilloDev <murillomarquesrespaw@gmail.com>
2024-12-02 12:01:36 -0300

    Add files via upload

---
+++ Trabalho Anselmo/js/javinha.js
@@ -0,0 +1,9 @@
+function enviar(){
+    const textarea = document.getElementById("feedback");
+    alert("Feedback recebido");
+    textarea.value = "";
+}
+
+function login(){
+    alert("Usuário logado");
+}
